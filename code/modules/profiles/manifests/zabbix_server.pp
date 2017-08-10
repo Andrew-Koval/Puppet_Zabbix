59e5c1981c4a85353e52bea35a6bfc2612cbadeb
Andriy Koval <andruxxxxa@i.ua>
2017-08-10 13:01:59 +0300

    Puppet_Zabbix

--- code/modules/profiles/manifests/zabbix_server.pp
+++ code/modules/profiles/manifests/zabbix_server.pp
@@ -2,7 +2,7 @@ class profiles::zabbix_server {
 
   $zabbix_src = hiera('profiles::zabbix_server::zabbix_server::zabbix::zabbix_src')
   $dbhost     = hiera('profiles::zabbix_server::zabbix_server::zabbix::dbhost')
-  
+
   class { 'zabbix_server::zabbix':
     zabbix_src => $zabbix_src,
     dbhost     => $dbhost,
@@ -11,5 +11,6 @@ class profiles::zabbix_server {
 
   class { 'zabbix_server::zabbix_configs':
     require => Class['zabbix_server::zabbix'],
+    notify  => Service['httpd'],
   }
 }
