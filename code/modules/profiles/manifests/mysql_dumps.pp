4c17ff0e54510153df2545018b628e4419b8b78c
Andriy Koval <andruxxxxa@i.ua>
2017-08-13 17:11:26 +0300

    Puppet_Zabbix

--- code/modules/profiles/manifests/mysql_dumps.pp
+++ code/modules/profiles/manifests/mysql_dumps.pp
@@ -4,7 +4,6 @@ class profiles::mysql_dumps {
   $source1  = hiera('profiles::mysql_database::mysql::mysql_files::source1')
   $source2  = hiera('profiles::mysql_database::mysql::mysql_files::source2')
   $source3  = hiera('profiles::mysql_database::mysql::mysql_files::source3')
-  $service  = hiera('profiles::mysql_database::mysql::mysql_files::service')
   $dump     = ["$source1", "$source2", "$source3"]
 
   notice ($dump)
@@ -17,10 +16,8 @@ class profiles::mysql_dumps {
   mysql::mysql_files {"$source":
     database => 'zabbix',
     source   => $source,
-    service  => $service,
   }
 
   notice("MySQL dump $source is uploaded")
 
   class { 'profiles::mysql_dumps': }
-
