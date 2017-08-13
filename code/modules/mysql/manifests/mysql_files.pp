07cb4b6386eb3e4e4a5a7cc31e154fc79c1690f0
Andriy Koval <andruxxxxa@i.ua>
2017-08-13 17:20:27 +0300

    Puppet_Zabbix

--- code/modules/mysql/manifests/mysql_files.pp
+++ code/modules/mysql/manifests/mysql_files.pp
@@ -1,10 +1,8 @@
 define mysql::mysql_files (
   $database = [],
   $source   = [],
-  $service  = [],
 ) {
   exec { "$source":
     command => "/usr/bin/mysql -uroot $database < $source",
-    notify  => Service["$service"],
   }
 }
