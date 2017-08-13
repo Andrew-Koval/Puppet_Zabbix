9785fc54fec7c88712a09d88de97463422844356
Andriy Koval <andruxxxxa@i.ua>
2017-08-13 23:47:22 +0300

    Puppet_Zabbix

--- code/modules/roles/manifests/server.pp
+++
@@ -1,8 +0,0 @@
-class roles::server {
-  include profiles::zabbix_server
-  include profiles::zabbix_database
-  include profiles::mysql_dumps
-  include profiles::httpd_server
-  include profiles::zabbix_agent
-}
-
