c6a7e45e30baf3d798c8ba28d70d7a5ebc162625
Andriy Koval <andruxxxxa@i.ua>
2017-08-13 16:57:01 +0300

    Puppet_Zabbix

--- code/modules/profiles/manifests/zabbix_database.pp
+++ code/modules/profiles/manifests/zabbix_database.pp
@@ -8,7 +8,7 @@ class profiles::zabbix_database {
     before => Mysql::Mysql_database['zabbix'],
   }
 
-  mysql::mysql_database {'zabbix':
+  mysql::mysql_database {"$db_name":
     db_name  => $db_name,
     user     => $user,
     password => $password,
