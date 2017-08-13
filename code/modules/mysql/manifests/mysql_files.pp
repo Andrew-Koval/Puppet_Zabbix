701d5252adf90a40f7ff7b35681ebec96f2469eb
Andriy Koval <andruxxxxa@i.ua>
2017-08-13 23:50:04 +0300

    Puppet_Zabbix

--- code/modules/mysql/manifests/mysql_files.pp
+++ code/modules/mysql/manifests/mysql_files.pp
@@ -1,9 +1,8 @@
 define mysql::mysql_files (
-  $database = 'zabbix',
+  $database = [],
   $source   = [], 
 ) {
   exec { "$source":
     command => "/usr/bin/mysql -uroot $database < $source",
-    notify   => Service['zabbix-server'],
   }
 }
