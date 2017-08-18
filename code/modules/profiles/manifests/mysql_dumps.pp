b7d340b1020dfb28ccb173820bf70e9452398010
Andriy Koval <andruxxxxa@i.ua>
2017-08-18 12:59:31 +0300

    Puppet_Zabbix

--- code/modules/profiles/manifests/mysql_dumps.pp
+++ code/modules/profiles/manifests/mysql_dumps.pp
@@ -1,14 +1,12 @@
 class profiles::mysql_dumps {
 
   $database = hiera('profiles::mysql_database::mysql::mysql_files::database')
-  $source1  = hiera('profiles::mysql_database::mysql::mysql_files::source1')
-  $source2  = hiera('profiles::mysql_database::mysql::mysql_files::source2')
-  $source3  = hiera('profiles::mysql_database::mysql::mysql_files::source3')
-  $dump     = ["$source1", "$source2", "$source3"]
+  $source   = hiera_array('profiles::mysql_database::mysql::mysql_files::source')
+  $dump     = ["$source"]
 
   notice ($dump)
 
-    $dump.each |$schemas| {
+    $dump.slice |$schemas| {
     mysql::mysql_files { $schemas:
       database => $database,
       source   => $schemas,
